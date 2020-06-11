d8ab6b3bbb03446419ce96e30657ce9f496a123f
deyRupak <rupakdey9999@gmail.com>
2020-06-11 11:07:40 +0530

    Updates

---
+++ precache-manifest.5f910c6a03cb0f4bbe4ea801cacefd9d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e20955249203d35a290f0d7c397c4d2e",
+    "url": "/react-landing-pages/index.html"
+  },
+  {
+    "revision": "71b3f8a8bc873e291e86",
+    "url": "/react-landing-pages/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "3daef5608db2f808ac23",
+    "url": "/react-landing-pages/static/js/2.addc8e6f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-landing-pages/static/js/2.addc8e6f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "71b3f8a8bc873e291e86",
+    "url": "/react-landing-pages/static/js/main.c40bc1a7.chunk.js"
+  },
+  {
+    "revision": "6bfdab10ebd53921351e",
+    "url": "/react-landing-pages/static/js/runtime-main.0816f685.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-landing-pages/static/media/logo.5d5d9eef.svg"
+  }
+]);

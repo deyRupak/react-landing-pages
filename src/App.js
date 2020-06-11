3570e5a087ee32a3fb1e2db0b70ef87100687242
deyRupak <rupakdey9999@gmail.com>
2020-06-11 13:46:13 +0530

    Initiate HomePage Theme

--- src/App.js
+++ src/App.js
@@ -2,22 +2,21 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import Grid from '@material-ui/core/Grid'
+
+import HomeGrid from './components/HomeGrid'
+
 function App() {
   return (
     <div className="App">
+
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+        <p className='hr-width'>
+          <img src={logo} className="App-logo" alt="logo" />
+          <hr/>
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+
+        <HomeGrid/>
       </header>
     </div>
   );

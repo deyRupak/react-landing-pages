ad95dc327364da3b43a7b73aa9ef5f3bcf289116
deyRupak <rupakdey9999@gmail.com>
2020-06-26 13:04:37 +0530

    Add Story

--- src/components/proj1/Story.js
+++ src/components/proj1/Story.js
@@ -1,12 +1,43 @@
 import React from 'react';
 import Paper from '@material-ui/core/Paper';
 import Avatar from '@material-ui/core/Avatar';
+import Grid from '@material-ui/core/Grid';
+
+import PlayCircleOutlineIcon from '@material-ui/icons/PlayCircleOutline';
+import IconButton from '@material-ui/core/IconButton';
+import Button from '@material-ui/core/Button';
 
 
 export default function Story(){
+    const c = [1,2,3,4,5,6,7,8]
     return(
-        <Paper style={{width: '90%', height: '16vh'}}>
-            <Avatar alt="Remy Sharp" src="/static/images/avatar/1.jpg" style={{ width: '80px', height: '80px' }} />
+        <Paper style={{ width: 'auto', height: 'auto', backgroundColor:'transparent'}} elevation={0}>
+            <Grid
+                container
+                direction="row"
+                justify="space-between"
+                alignItems="center"
+                style={{color: 'white'}}
+            >
+            <Grid item style={{ fontSize: 20 }}><b>Stories</b></Grid>
+            <Grid item>
+                        <Button
+                            startIcon={<PlayCircleOutlineIcon />}
+                            style={{
+                                background: 'transparent',
+                                color: 'white',
+                                textTransform: 'none'
+                            }}>Watch All</Button>
+                </Grid>
+            </Grid>
+
+            <Grid container justify="center" alignItems="center" >
+                {c.map((value) => (
+                    <Grid Item style={{padding:20}}>
+                        <Avatar alt={value} src="/static/images/avatar/1.jpg" style={{ width: '80px', height: '80px' }} />
+                    </Grid>
+                ))}
+            </Grid>
         </Paper>
     )
 }
